49a12b424c35538fb1272dfff543585c15f5f588
gabrielszz <50212683+gabrielszz@users.noreply.github.com>
2021-05-14 14:47:27 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<article class="conteudo conteudo-index">
+						<div style="text-align: center; padding: 30px; overflow: hidden;">
+						
+						<a rel="bookmark" class="btn-cuidados bt-cuidado" title="Apêndicite">Apêndicite</a>
+
+						<a rel="bookmark" class="btn-cuidados2 bt-cuidado" title="DRGE">DRGE</a>
+
+						<a rel="bookmark" class="btn-cuidados3 bt-cuidado" title="DRGE">Úlcera Gástrica</a>
+
+						<a rel="bookmark" class="btn-cuidados4 bt-cuidado" title="DRGE">Pancreatite</a>
+
+						
+						</div>
+
+
+						<div class="table colunas-conteudo">
+							<div class="cuidados-conteudo">
+								
+							</div>
+						</div>
+
+
+					</article>
