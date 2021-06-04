4ee92ede068ad19314c0dce3df3ae6f6061953fd
gabrielszz <50212683+gabrielszz@users.noreply.github.com>
2021-06-04 11:36:57 -0300

    Create cadastraProdutos.php

---
+++ projetosnovo/cadastraProdutos.php
@@ -0,0 +1,19 @@
+$acao = $_POST['acao'];
+//acao 1 - cadastrar, 2 - alterar, 
+
+$id = $_POST['id'];
+$nome = $_POST['nome'];
+$status = $_POST['status'];
+$preco = $_POST['preco'];
+$descricao = $_POST['descricao'];
+$categoria = $_POST['id_categoria'];
+
+functionCadastraProduto(){
+  #sql = "Insert into PRODUTOS(nome, status, preco, descricao, categoria)values('$nome', '$status', '$preco', '$descricao', '$categoria');
+}
+functionAlteraProduto(){
+  $sql = "Update PRODUTOS set nome = '$nome'
+}
+functionExcluiProduto(){
+  $sql = "Delete from PRODUTOS where id like '$id';
+}
